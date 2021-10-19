e07974135c6db9b8ab324157f77974a114e20a67
pattyzamsal <pazs1991@gmail.com>
2021-10-19 00:03:02 -0500

    Refactor initialViewController without any view inside storyboard

--- Sources/Initial/InitialViewController.swift
+++ Sources/Initial/InitialViewController.swift
@@ -9,15 +9,40 @@ import Foundation
 import UIKit
 
 class InitialViewController: BaseViewController {
-    @IBOutlet private weak var titleLabel: UILabel!
-    @IBOutlet private weak var descriptionLabel: UILabel!
-    @IBOutlet private weak var startButton: UIButton!
+    private let titleLabel: UILabel = {
+        let titleLabel = PostLabel()
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.assignStyleInInitialView(type: .title)
+        return titleLabel
+    }()
+    
+    private let descriptionLabel: UILabel = {
+        let descriptionLabel = PostLabel()
+        descriptionLabel.translatesAutoresizingMaskIntoConstraints = false
+        descriptionLabel.assignStyleInInitialView(type: .description)
+        return descriptionLabel
+    }()
+    
+    private var stackView: UIStackView!
+    
+    private let startButton: UIButton = {
+        let startButton = UIButton(type: .custom)
+        startButton.titleLabel?.font = UIFont.getFontApp(style: .heavy, size: .medium)
+        startButton.backgroundColor = .clear
+        startButton.translatesAutoresizingMaskIntoConstraints = false
+        return startButton
+    }()
     
     private lazy var presenter: InitialContract.Presenter = {
         let navigator = InitialNavigator(viewController: self)
         return InitialPresenter(view: self,
                                 navigator: navigator)
     }()
+    
+    override func loadView() {
+        super.loadView()
+        createViews()
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,15 +59,49 @@ class InitialViewController: BaseViewController {
         navigationController?.setNavigationBarHidden(false, animated: animated)
     }
     
-    @IBAction func didTapStartButton(_ sender: Any) {
+    @objc func didTapStartButton(_ sender: Any) {
         presenter.didTapStartButton()
     }
 }
 
 private extension InitialViewController {
+    func createViews() {
+        view.backgroundColor = UIColor.getColorApp(name: .blueLight)
+        createStackView()
+        view.addSubview(stackView)
+        view.addSubview(startButton)
+        addConstraintsForViews()
+    }
+    
+    func createStackView() {
+        let views = [titleLabel, descriptionLabel]
+        titleLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        descriptionLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        let stackView = UIStackView(arrangedSubviews: views)
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.spacing = 10
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        self.stackView = stackView
+    }
+    
+    func addConstraintsForViews() {
+        NSLayoutConstraint.activate([
+            stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 30),
+            stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -30),
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalToSystemSpacingBelow: view.centerYAnchor, multiplier: 0.9),
+            
+            startButton.topAnchor.constraint(equalTo: stackView.bottomAnchor, constant: 30),
+            startButton.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+    }
+    
     func setupView() {
         titleLabel.text = TextsConstants.initialTitle.rawValue
         descriptionLabel.text = TextsConstants.descriptionApp.rawValue
+        startButton.addTarget(self, action: #selector(didTapStartButton), for: .touchUpInside)
         startButton.setTitle(TextsConstants.startButtonTitle.rawValue, for: .normal)
     }
 }
